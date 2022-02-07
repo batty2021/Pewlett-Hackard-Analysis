df161693b222054ba4c1ec29d5cba22074db17fc
betty <battyalex1510@gmail.com>
2022-02-07 13:48:01 -0600

    adding modified schema.sql

--- schema.sql
+++ schema.sql
@@ -46,14 +46,12 @@ CREATE TABLE dept_employee (
 	PRIMARY KEY (emp_no,dept_no)
 );
 
-CREATE TABLE titles ( 
-	emp_no INT NOT NULL,
-	title VARCHAR(167) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_No),
-	UNIQUE (title)
- );
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
  
- SELECT *
- FROM departments;
