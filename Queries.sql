5cc72388593f6abe5a83e3211fbf1668cb24d3de
betty <battyalex1510@gmail.com>
2022-02-08 15:21:01 -0600

    updating new queries

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,5 @@
 
- 
+
  SELECT *
  FROM salaries;
  
@@ -68,12 +68,13 @@ retirement_info.last_name,
 	FROM retirement_info
 	LEFT JOIN dept_employee
 	ON retirement_info.emp_no = dept_employee.emp_no;
+
 SELECT ri.emp_no,
     ri.first_name,
 ri.last_name,
     de.to_date
 	FROM retirement_info as ri
-LEFT JOIN dept_emp as de
+LEFT JOIN dept_employee as de
 ON ri.emp_no = de.emp_no;
 
 SELECT d.dept_name,
@@ -93,4 +94,67 @@ FROM retirement_info as ri
 LEFT JOIN dept_employee as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
-	
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_employee as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no,
+    first_name,
+last_name,
+    gender
+	INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+	INTO emp_info
+FROM employees as e
+INNER JOIN dept_employee as de
+ ON (e.emp_no = de.emp_no)
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+	 
+	-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+--list of depa		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employee AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
